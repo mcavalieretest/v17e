c4a882096732ae9698417384605472789dfa6b54
lsargent <lsargent@821e859f-0beb-46bc-aa8d-796bf9038c96>
2013-11-26 17:31:21 +0000

    Initial commit

---
+++ sites/social-business/js/code.js
@@ -0,0 +1,54 @@
+dojo.ready(function(){
+	sbi.hp.init();
+})
+
+
+var sbi = sbi || {};
+sbi.hp = sbi.hp || {};
+sbi.twitterWidget || {};
+
+
+sbi.hp = {
+	init : function(){
+		sbi.twitterWidget.getTweets();
+		this.contentWidget();
+	
+	},
+
+	contentWidget : function(){
+		var _curr_ad = 'nav_main', 
+		_buttons = dojo.query('.ibm_cci--toggle-widget-nav li a');
+		_buttons.forEach(function(_obj, _index){
+		    dojo.connect(_obj, 'click', function(e){
+		    	
+		        var _rel = dojo.attr(_obj, 'id');
+		        if(_rel != _curr_ad){
+		        	dojo.attr(dojo.query('.ibm_cci--toggle-widget-nav li a.selected')[0], 'aria-selected', 'false');
+		            dojo.removeClass(dojo.query('.ibm_cci--toggle-widget-nav li a.selected')[0], 'selected');
+		            dojo.removeClass(dojo.query('.ibm_cci--toggle-widget-content.selected')[0], 'selected');
+		             var _new_piece = dojo.byId(_rel);
+		              dojo.addClass(_new_piece, 'selected');
+		             _rel = _rel.replace('nav_', 'sbi-blog-list_');
+		             _new_piece = dojo.byId(_rel);
+		            dojo.addClass(_new_piece, 'selected');
+		            _curr_ad = _rel;
+		        }
+		        e.preventDefault();
+		    });
+		});
+	}
+}
+
+sbi.twitterWidget = {
+	getTweets : function(){
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+	
+	}
+}
+
+
+
+
+
+
+
