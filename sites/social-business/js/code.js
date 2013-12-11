c821bdf5ce8099fceabe296408147578e0ca03cc
lsargent <lsargent@821e859f-0beb-46bc-aa8d-796bf9038c96>
2013-12-11 16:18:59 +0000

    switched test twitter widget to @IBMSocialBiz account

--- sites/social-business/js/code.js
+++ sites/social-business/js/code.js
@@ -54,8 +54,8 @@ sbi.detailView = {
 
 sbi.twitterWidget = {
 	getTweets : function(){
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-	
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
 	}
 }
 
@@ -68,8 +68,6 @@ sbi.doArchives = {
 		});
 	}
 }
-
-
 
 
 
