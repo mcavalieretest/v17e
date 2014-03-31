328f59bdf71b096c27ed7d12f159079ed6df8d9d
clarkk <clarkk@821e859f-0beb-46bc-aa8d-796bf9038c96>
2014-03-31 21:33:56 +0000

    Put scripts back on HP.

--- sites/mainframe50/js/ww.js
+++ sites/mainframe50/js/ww.js
@@ -167,17 +167,6 @@ var checkMLinksExist = setInterval(function() {
 
 });
 
-addthisevent.settings({
-	mouse		: false,
-	css			: false,
-	outlook		: {show:true, text:"Outlook Calendar"},
-	google		: {show:true, text:"Google Calendar"},
-	yahoo		: {show:true, text:"Yahoo Calendar"},
-	ical		: {show:true, text:"iCal Calendar"},
-	hotmail		: {show:true, text:"Hotmail Calendar"},
-	facebook	: {show:true, text:"Facebook Calendar"}
-});
-
 /**
  * mlpushmenu.js v1.0.0
  * http://www.codrops.com
