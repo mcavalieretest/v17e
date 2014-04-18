09673c71012962d51fe6c625d026e8836db5dc4c
Mike Cavaliere <mike@cavaliere.org>
2014-04-18 17:14:29 -0400

    Sample grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        // files: {
+        //     'js/build/ww.min.js': ['js/src/ww.js']
+        // }
+        files: {
+          'js/ww.min.js': ['js/ww.js']
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
