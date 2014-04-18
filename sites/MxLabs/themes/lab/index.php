69d6580ba0abaa3fa46028f09649a8effd44826d
jeffdos <jeffdos@821e859f-0beb-46bc-aa8d-796bf9038c96>
2014-04-18 18:28:35 +0000

    update tagline

--- sites/MxLabs/themes/lab/index.php
+++ sites/MxLabs/themes/lab/index.php
@@ -11,7 +11,7 @@
                             <div class="ibm-col-1-1">                                
                                 <div class="ibm-col-6-3 feature-box">
                                     <h2 class="section-header">Welcome to the IBM Marketing Experience Labs</h2>
-                                    <p><span style="opacity: 1">We&rsquo;re here to share our experiences with you—the successes, the failures, and the lessons we learn along the way.</span></p>
+                                    <p><span style="opacity: 1">We started off as a pilot team in 2011, launched a digital design lab in 2012, and grew to 24 teams with 160 people by 2013. This year we’ve expanded globally and opened three new Labs in Tokyo, London, and Bangalore. We’re here to share our experiences with you—the successes, the failures, and the lessons we learn along the way. </span></p>
                                 </div>
                             </div>
                         </div>
@@ -19,7 +19,7 @@
                             <div class="ibm-col-1-1">
                                 <div class="ibm-col-6-3 feature-box">
                                     <h2 class="section-header">Moving in with Watson</h2>
-                                    <p><span style="opacity: 1">As a monolith a little out of its comfort zone, it’s hard not to feel a kind of kinship with 51 Astor Place.</span></p>
+                                    <p><span style="opacity: 1">IBM doesn’t exactly see itself as the kind of company that belongs in an unconventional neighborhood. But as a monolith a little out of its comfort zone, it’s hard not to feel a kind of kinship with 51 Astor Place.</span></p>
                                 </div>
                             </div>
                         </div>
@@ -27,7 +27,7 @@
                             <div class="ibm-col-1-1">
                                 <div class="ibm-col-6-3 feature-box">
                                     <h2 class="section-header">Building a Lab in Tokyo</h2>
-                                    <p><span style="opacity: 1">At the Marketing Experience Labs in NYC, we are used to a little bit of chaos accompanying new things.</span></p>
+                                    <p><span style="opacity: 1">Carving out a space for a Marketing Experience Lab in a well established IBM facility is no small feat. This is especially true if you don’t speak the language, are unfamiliar with IBM outside of our rather unusual Lab setting, and have the added stress of not knowing where you are going to stay for the next six months. </span></p>
                                 </div>
                             </div>
                         </div>
