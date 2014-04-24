9bdb173eade4c15a0516333682777480e092e165
Mike Cavaliere <mike@cavaliere.org>
2014-04-24 14:48:15 -0400

    Experimenting with different build targets and required CLI parameters

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,16 @@ module.exports = function(grunt) {
               + '// URI: www.ibm.com/common/v17e/js/ww.js\n'
               + '// Description: Official JS file for v17e project\n'
               + '//\n';
-  
+
+  var CORE_LIBS_PATHS = [
+    'js/src/ibm/common/lib/modernizr.js',
+    'js/src/ibm/common/util/dojo-loader.js',
+    'js/src/ibm/common/lib/jquery.js',
+    'js/src/ibm/common/util/ibm-core.js',
+    'js/src/ibm/common/lib/mlpushmenu.jquery.js',
+    'js/src/ibm/common/widget/accordion.js',
+    'js/src/ibm/ww/init.js'
+  ];
 
   // Project configuration.
   grunt.initConfig({
@@ -20,18 +29,15 @@ module.exports = function(grunt) {
       options: {
         separator: ";\n\n"
       },
-      ww: {
+      dev: {
         nonull: true,
-        src: [
-          'js/src/ibm/common/lib/modernizr.js',
-          'js/src/ibm/common/util/dojo-loader.js',
-          'js/src/ibm/common/lib/jquery.js',
-          'js/src/ibm/common/util/ibm-core.js',
-          'js/src/ibm/common/lib/mlpushmenu.jquery.js',
-          'js/src/ibm/common/widget/accordion.js',
-          'js/src/ibm/ww/init.js'
-        ],
-        dest: 'js/build/ww.js'
+        src: CORE_LIBS_PATHS,
+        dest: 'js/build/ww.dev.js'
+      },
+      prod: {
+        nonull: true,
+        src: CORE_LIBS_PATHS,
+        dest: 'js/build/ww.prod.js'
       }
     },
 
@@ -41,19 +47,29 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          'js/build/ww.min.js': ['js/build/ww.js']
+          'js/build/ww.prod.min.js': ['js/build/ww.prod.js']
         }
       }
     }
   });
 
+  var target = grunt.option("target");
+
+  if (typeof(target) == "undefined") {
+    grunt.fail.warn("A target must be specified using the --target=[target] option. Valid targets are \"dev\" and \"prod\".");
+  }
 
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat']);  //, 'uglify'
+  var taskList = ['concat:'+target];
 
+  if (target == "prod") {
+    taskList.push("uglify");
+  }
+
+  // Default task(s).
+  grunt.registerTask('default', taskList);  //, 'uglify'
 };
