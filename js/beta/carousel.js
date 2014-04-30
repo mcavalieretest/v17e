3cb2585cca20321b907c891ca6c47c1aca6c6bc4
mcavaliere <mcavaliere@821e859f-0beb-46bc-aa8d-796bf9038c96>
2014-04-30 19:37:24 +0000

    Basic prev/next

--- js/beta/carousel.js
+++ js/beta/carousel.js
@@ -1,31 +1,80 @@
 (function($, IBM) {
-  IBM.ns(IBM, "IBM.Common.Widgets.Carousel");
-  IBM.ns(IBM, "IBM.Common.Util");
+  IBM.ns(IBM, "Common.Widgets.Carousel");
+  IBM.ns(IBM, "Common.Util");
 
   IBM.Common.Util.getMaxHeight = function() {
-    
+
   };
 
   IBM.Common.Widgets.Carousel = function(options) {
     var defaults = {
-
+      prevButtonSelector: ".ibm-ribbon-prev",
+      nextButtonSelector: ".ibm-ribbon-next",
+      scrollContainerSelector: ".ibm-ribbon-section",
+      panelContainerSelector:  ".ibm-columns"
     };
 
+    // Save the important elements
     this.config  = $.extend({}, defaults, options);
     this.element = $(this.config.selector);
+    this.prevButton = $(this.config.prevButtonSelector);
+    this.nextButton = $(this.config.nextButtonSelector);
+    this.scrollContainer = $(this.config.scrollContainerSelector);
 
-    this.initHeight();
+    this.initDimensions();
+    this.initEvents();
+
+    //Temporary
+    this.prevButton.show();
+    this.nextButton.show();
   };
 
   $.extend(IBM.Common.Widgets.Carousel.prototype, {
-    initHeight: function() {
-      this.element.height
+    initEvents: function() {
+      var self = this;
+
+      this.prevButton.on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        self.prev();
+      });
+
+      this.nextButton.on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        self.next();
+      });
+    },
+
+    initDimensions: function() {
+      this.scrollContainer.css("left", "0px");
+
+      this.panelWidth = this.element.find(this.config.panelContainerSelector+":first").width();
+    },
+
+    getCurrentSlidePos: function() {
+      return parseInt(this.scrollContainer.css("left").replace("px", ""));
+    },
+
+    next: function() {
+      var currentLeft = this.getCurrentSlidePos(),
+          newLeft     = currentLeft - this.panelWidth;
+
+      this.scrollContainer.animate({"left": newLeft}, 1000);
+    },
+
+    prev: function() {
+      var currentLeft = this.getCurrentSlidePos(),
+          newLeft     = currentLeft + this.panelWidth;
+      
+      this.scrollContainer.animate({"left": newLeft}, 1000);
     }
   });
 
-
+  $(function() {
+    window.carousel = new IBM.Common.Widgets.Carousel({
+      selector: "#my-carousel"
+    });
+  });
 })(jQuery, CHICKENFEED);
 
-$(function() {
-
-});
