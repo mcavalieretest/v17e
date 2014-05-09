5be18b5547c51544116f7c0657dbd882b3f86e65
Mike Cavaliere <mike@cavaliere.org>
2014-05-09 16:24:17 -0400

    Adding hook handler to this branch

---
+++ github.php
@@ -0,0 +1,33 @@
+<?php 
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+?>
+
+<h2>Output: </h2>
+
+<?php 
+try
+{
+  if (array_key_exists("payload", $_REQUEST)) {
+    echo "Payload: <br>";
+    $payload = json_decode($_REQUEST['payload']);
+    print_r($payload);
+
+    if ($payload->ref === 'refs/heads/hooktest') {
+      // path to your site deployment script
+      echo exec('git pull origin stripped');
+    }
+  }
+}
+catch(Exception $e)
+{
+  echo "Exception: <br>";
+  print_r($e);
+  exit(0);
+}
+
+
+
+?>
+
