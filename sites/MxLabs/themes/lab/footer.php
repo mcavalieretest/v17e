ca1d36ed21a6503d3d02cef4d8d62fb8345ae3b9
vouyoung <vouyoung@821e859f-0beb-46bc-aa8d-796bf9038c96>
2014-05-13 20:08:06 +0000

    matching wp-content structure

---
+++ sites/MxLabs/themes/lab/footer.php
@@ -0,0 +1,32 @@
+
+			<!--! FOOTER_BEGIN -->
+			<footer id="ibm-footer" role="contentinfo" aria-label="Footer">
+				<h2 class="ibm-access">Footer links</h2>
+				<ul>
+					<li><a href="http://www.ibm.com/contact/us/en/?lnk=flg-cont-usen">Contact</a></li>
+					<li><a href="http://www.ibm.com/privacy/us/en/?lnk=flg-priv-usen">Privacy</a></li>
+					<li><a href="http://www.ibm.com/legal/us/en/?lnk=flg-tous-usen">Terms of use</a></li>
+					<li><a href="http://www.ibm.com/accessibility/us/en/?lnk=flg-acce-usen">Accessibility</a></li>
+				</ul>
+			</footer>
+			<!-- FOOTER_END -->
+		</div>
+		<!-- IBM_TOP END -->
+
+		<!--! SCRIPTS_BEGIN -->		
+		<script src="/wp-content/themes/lab/assets/js/ww.min.js" type="text/javascript"></script>
+		<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/ww.js" type="text/javascript"></script> -->
+		
+		<!-- Slick carousel -->
+		<script src="<?php echo get_template_directory_uri(); ?>/assets/js/slick.min.js" type="text/javascript"></script>
+		
+		<!-- Analytics -->
+		<div id="ibm-metrics">
+			<!--<script src="//www.ibm.com/common/stats/stats.js" type="text/javascript">//</script>-->
+		</div>
+
+		<!-- WP scripts -->
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ sites/MxLabs/themes/lab/index.php
@@ -0,0 +1,65 @@
+            <?php get_header(); ?>
+
+            <!--! LEADSPACE_BEGIN -->
+            <section id="ibm-access-cntr" role="main">                
+                <div id="ibm-leadspace-head" class="ibm-container ibm-alternate-background ibm-ribbon">
+                    <header>
+                       <!-- <h1 class="mxlabs"><span>IBM</span> Marketing <br />Experience Labs</h1>-->
+                        <div class="mxlabs logo"></div>
+                    </header>
+                    <div class="corner"></div>
+                    <div id="ibm-leadspace-body" class="ibm-container-body">
+                     <div class="ibm-columns" id="ibm-lead-1">
+                            <div class="ibm-col-1-1">
+                                <div class="ibm-col-6-3 feature-box">
+                                    <h2 class="section-header">Moving in with Watson</h2>
+                                    <p><a href="#post-38">Located right in between the East Village and NYU, we'll be in a brand new twelve story glass and steel building. The new space is encircled by floor to ceiling windows, so there is no question of being relegated to a dark corner. Read more...</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ibm-columns" id="ibm-lead-2">
+                            <div class="ibm-col-1-1">                                
+                                <div class="ibm-col-6-3 feature-box">
+                                    <h2 class="section-header">The Story of Our Lab</h2>
+                                    <p><a href="#post-25">We started as a single pilot team in 2011, and became the Design Lab in 2012. In 2013 we grew to 24 teams with 160 people; this year we are expanding globally with three new Labs in Tokyo, London, and Bangalore. We're here to share our experiences with you&#151the successes, the failures, and the lessons we learn along the way. Read more...</a></p>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <div class="ibm-columns" id="ibm-lead-3">
+                            <div class="ibm-col-1-1">
+                                <div class="ibm-col-6-3 feature-box">
+                                    <h2 class="section-header">Building a Lab in Tokyo</h2>
+                                    <p><a href="#post-21">Carving out a space for a Marketing Experience Lab in a well established IBM facility is no small feat. This is especially true if you don’t speak the language, are unfamiliar with IBM outside of our rather unusual Lab setting, and have the added stress of not knowing where you are going to stay for the next six months. Read more...</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- LEADSPACE_END -->
+            
+            <!-- CONTENT_BEGIN -->
+            <section id="ibm-pcon">                
+                <div id="ibm-content">
+                    <!-- CONTENT_BODY -->
+                    <div id="ibm-content-body">
+                        <div id="ibm-content-main">
+                            <div class="ibm-columns">
+                                <section class="ibm-col-6-4 large">
+                                    <?php get_template_part('loop'); ?>
+                                    <?php get_template_part('pagination'); ?>
+                                </section>                                
+                            </div>
+                        </div>
+                    </div>
+                    <!-- CONTENT_BODY_END -->
+                </div>                
+                
+                <!-- NAVIGATION_BEGIN -->
+                <nav id="ibm-navigation"></nav>
+                <!-- NAVIGATION_END -->
+            </section>
+            <!-- CONTENT_END -->
+
+            <?php get_footer(); ?>
