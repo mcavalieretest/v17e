00330201a6bfc748d265efc898ac4e5c4f1fe21c
mcavaliere <mcavaliere@821e859f-0beb-46bc-aa8d-796bf9038c96>
2014-05-16 16:51:58 +0000

    JS test runner and specs for carousel

---
+++ spec/spec/CarouselSpec.js
@@ -0,0 +1,261 @@
+(function($, IBM){
+  // jasmine.getEnv().addReporter(new jasmine.ConsoleReporter(console.log));
+
+  describe("IBM.Common.Widgets.Carousel", function() {
+    var Carousel = IBM.Common.Widgets.Carousel,
+        carousel;
+
+    beforeEach(function() {
+      $(document.body).append('<div id="carousel-1"></div>');
+      carousel = new Carousel($("#carousel-1"), {init: false});
+    });
+
+    afterEach(function() {
+      $("#carousel-1").remove();
+
+      carousel = null;
+    })
+
+    it("should not be null", function() {
+      expect(carousel).not.toBeNull();
+    });
+
+    describe("constructor", function() {
+      it("should save slide data", function() {
+        $(document.body).append('<div id="constructor-carousel"></div>');
+
+        var c1 = new Carousel($("#constructor-carousel"), {
+          init: false, 
+          slides: [
+            {
+              items: [
+                {content: "foo"},
+                {content: "bar"},
+                {content: "baz"}
+              ]
+            }
+          ]
+        });
+
+        expect(c1.data).not.toBeUndefined();
+        expect(c1.data.slides).not.toBeUndefined();
+        expect(c1.data.slides[0].items).not.toBeUndefined();
+
+        $("#constructor-carousel").remove();        
+      });
+    });
+
+    describe("slide / item methods", function() {
+      var c1, c2, c3;
+
+      beforeEach(function() {
+        $(document.body).append('<div id="si-methods-1"></div>');
+        $(document.body).append('<div id="si-methods-2"></div>');
+        $(document.body).append('<div id="si-methods-3"></div>');
+
+          c1 = new Carousel($("#si-methods-1"), {
+            init: false, 
+            slides: [
+              {items: [{content: "foo"}]}
+            ]
+          });
+          c2 = new Carousel($("#si-methods-2"), {
+            init: false, 
+            slides: [            
+              {items: [{content: "foo"}]},
+              {items: [{content: "foo"}, {content: "foo"}]}
+            ],
+          });
+          c3 = new Carousel($("#si-methods-3"), {
+            init: false, 
+            slides: [            
+              {items: [{content: "foo"}]},
+              {items: [{content: "foo"}, {content: "foo"}]},
+              {items: [{content: "foo"}, {content: "foo"}, {content: "foo"}]}
+            ]
+          });
+      });
+
+      afterEach(function() {
+        $("#si-methods-1").remove();
+        $("#si-methods-2").remove();
+        $("#si-methods-3").remove();
+      });
+
+
+      describe("lastSlideIndex()", function() {
+        it("should return an accurate index", function() {
+          expect(c1.lastSlideIndex()).toEqual(0);
+          expect(c2.lastSlideIndex()).toEqual(1);
+          expect(c3.lastSlideIndex()).toEqual(2);
+        });
+      });
+
+      describe("addItem()", function() {
+        beforeEach(function() {
+          expect(c3.data.slides.length).toEqual(3);
+          expect(c3.data.slides[0].items.length).toEqual(1);
+          expect(c3.data.slides[1].items.length).toEqual(2);
+          expect(c3.data.slides[2].items.length).toEqual(3);
+        });
+
+        afterEach(function() {
+          expect(c3.data.slides.length).toEqual(3);
+        });
+
+        it("should add an item to the first slide", function() {
+          c3.addItem({content: "foo"}, 0);
+          
+          expect(c3.data.slides[0].items.length).toEqual(2);
+          expect(c3.data.slides[1].items.length).toEqual(2);
+          expect(c3.data.slides[2].items.length).toEqual(3);
+        });
+
+        it("should add an item to the third slide", function() {
+          c3.addItem({content: "foo"}, 2);
+
+          expect(c3.data.slides[0].items.length).toEqual(1);
+          expect(c3.data.slides[1].items.length).toEqual(2);
+          expect(c3.data.slides[2].items.length).toEqual(4);
+        });
+      });
+
+      describe("addItems()", function() {
+        it("should add 1 item to the 2nd slide", function() {
+          var l = c3.data.slides[1].items.length
+
+          c3.addItems([
+            {content: "foo"}
+          ], 1);
+
+          expect(c3.data.slides[1].items.length).toEqual(l+1);
+        });
+
+        it("should add 4 items to the 3rd slide", function() {
+          var l = c3.data.slides[2].items.length
+
+          c3.addItems([
+            {content: "foo"},
+            {content: "foo"},
+            {content: "foo"},
+            {content: "foo"}
+          ], 2);                    
+
+          expect(c3.data.slides[2].items.length).toEqual(l+4);
+        });
+      });
+
+      describe("addSlide()", function() {
+        it("should add one slide", function() {
+          var l = c3.data.slides.length;
+
+          c3.addSlide({items: [{content: "foo"}]});
+          
+          expect(c3.data.slides.length).toEqual(l+1);
+        });
+      });
+
+      describe("addSlides()", function() {
+        it("should add one slide", function() {
+          var l = c3.data.slides.length;
+
+          c3.addSlides([
+            { items: [{content: "foo"}] }
+          ]);
+          
+          expect(c3.data.slides.length).toEqual(l+1);
+        });
+
+        it("should add 3 slides", function() {
+          var l = c3.data.slides.length;
+
+          c3.addSlides([
+            { items: [{content: "foo"}] },
+            { items: [{content: "foo"}] },
+            { items: [{content: "foo"}] }
+          ]);
+          
+          expect(c3.data.slides.length).toEqual(l+3);
+        });
+
+      });
+    });
+
+    describe("buildHtml", function() {
+      var c1;
+
+      beforeEach(function() {
+        $(document.body).append('<div id="buildhtml-carousel"></div>');
+
+        c1 = new Carousel($("#buildhtml-carousel"), {init: false});
+      });
+
+      afterEach(function() {
+        $("#buildhtml-carousel").remove();
+        c1 = null;
+      });
+
+      it("should create one slide", function() {
+        c1.addSlide({
+          items: [
+            {content: "aaaaa"},
+            {content: "bbbbb"},
+            {content: "ccccc"}
+          ]
+        });
+
+        c1.buildHtml();
+
+        expect(c1.element.find(".ibm-columns").length).toEqual(1);
+      });
+
+      it("should create a single slide", function() {
+        c1.addSlide({
+          items: [
+            {content: "aaaaa"},
+            {content: "bbbbb"},
+            {content: "ccccc"}
+          ]
+        });
+
+        c1.buildHtml();
+
+        expect(c1.element.find(".ibm-columns").length).toEqual(1);
+      });
+
+      it("should create 3 slides", function() {
+        c1.addSlides([
+          {items: [{content: "aaaaa"}]},
+          {items: [{content: "aaaaa"}]},
+          {items: [{content: "aaaaa"}]}
+        ]);
+
+        c1.buildHtml();
+
+        expect(c1.element.find(".ibm-columns").length).toEqual(3);
+      });
+
+      it("should apply the proper layout classes", function() {
+        c1.addSlides([
+          {items: [{content: "aaaaa"}]},
+          {items: [{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"}]},
+          {items: [{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"}]},
+          {items: [{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"},{content: "aaaaa"}]}
+        ]);
+
+        c1.buildHtml();
+
+        var slides = c1.element.find(".ibm-columns");
+
+        expect(slides.eq(0).find(".ibm-col-1-1").length).toEqual(1);
+        expect(slides.eq(1).find(".ibm-col-6-2").length).toEqual(3);
+        expect(slides.eq(2).find(".ibm-col-5-1").length).toEqual(5);
+        expect(slides.eq(3).find(".ibm-col-6-1").length).toEqual(6);
+      });
+
+
+    });
+
+
+  });
+})(jQuery, CHICKENFEED);
