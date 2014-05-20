8fcb1643c09bd2ffef2341881a14f913690da114
vchouhan <vchouhan@821e859f-0beb-46bc-aa8d-796bf9038c96>
2014-05-20 21:39:13 +0000

    Updated the admin with modal windows for delete, edit and select menu

--- sites/voices/js/voicesAdmin.js
+++ sites/voices/js/voicesAdmin.js
@@ -4,7 +4,14 @@ $(function() {
 		$addAccount = $('a.ibm_cci_addAccount, img.ibm_cci_addAccount, p.ibm_cci-addnewAcc'),
 		$addAcountBlock = $('.ibm-band.ibm_cci__ibm-band'),
 		$addAccountTarget = $('a.ibm_cci_addAccount, img.ibm_cci_addAccount'),
-		$pAddnewAcc = $('p.ibm_cci-addnewAcc');
+		$pAddnewAcc = $('p.ibm_cci-addnewAcc'),
+		$editIcon = $("img.ibm_cci-editIcon"),
+		$editModalWin = $('#ibm_cci-modalWin'),
+		$pconDiv = $('.ibm_cci__ibm-pcon'),
+		$cancelEditModalWin = $('.ibm_cci-deleteButtGray'),
+		$deleteButton = $('.ibm_cci-datacell p.ibm_cci-deleteButt a'),
+		$deleteModalWin = $('#ibm_cci-modalDelWin');
+
 
 	// ADD ACCOUNT ONCLICK
 	$addAccount.on('click', function(event){
@@ -27,4 +34,57 @@ $(function() {
 		$addAccountTarget.css({'cursor': 'pointer'});
 		$pAddnewAcc.removeClass('ibm_cci-addnewAccClicked');
 	});
+
+	// EDIT BUTTON
+	$editIcon.on('click', function(event){
+		event.preventDefault();
+		$editModalWin.removeClass('ibm_cci_modalWinToggle');
+		$pconDiv.addClass('ibm_cci_modalIsOpen');
+		$('html').css('overflow', 'hidden');
+		hideEditDelete();	
+	});
+	// CANCEL EDIT BUTTON
+	$cancelEditModalWin.on('click', function(event){
+		event.preventDefault();
+		$editModalWin.addClass('ibm_cci_modalWinToggle');
+		$pconDiv.removeClass('ibm_cci_modalIsOpen');
+		$('html').removeAttr('style');
+		showEditDelete();	
+	});
+
+	// DELETE BUTTON
+	$deleteButton.on('click', function(event){
+		event.preventDefault();
+		$deleteModalWin.removeClass('ibm_cci_modalWinToggle');
+		$pconDiv.addClass('ibm_cci_modalIsOpen');
+		$('html').css('overflow', 'hidden');
+		hideEditDelete();
+	});
+
+	$cancelEditModalWin.on('click', function(event){
+		event.preventDefault();
+		$deleteModalWin.addClass('ibm_cci_modalWinToggle');
+		$pconDiv.removeClass('ibm_cci_modalIsOpen');
+		$('html').removeAttr('style');
+		showEditDelete();
+	});
+
+	var hideEditDelete = function(){
+		$editIcon.each(function(i){
+			$(this).css('display', 'none');
+		});
+		$deleteButton.each(function(i){
+			$('.ibm_cci-datacell p.ibm_cci-deleteButt').css('display', 'none');
+		});	
+	};
+
+	var showEditDelete = function(){
+		$editIcon.each(function(i){
+			$(this).removeAttr('style');
+		});
+		$deleteButton.each(function(i){
+			$('.ibm_cci-datacell p.ibm_cci-deleteButt').removeAttr('style');
+		});		
+	};
+
 });
